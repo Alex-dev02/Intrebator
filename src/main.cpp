576cab0116cdf294f40c65face164e0251ded828
Alex-dev02 <ionut02alex@yahoo.ro>
2022-11-09 21:42:00 +0200

    removed sqlite_orm from cmake project

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
-#include <sqlite_orm/sqlite_orm.h>
 
-
-#include <db/include/Database.hpp>
 #include <db/entities/User.hpp>
 
 #include <string>
 
 int main() {
-    User u{1, "Alex"};
-    DB::m_storage.replace(u);
     std::cout << "Hello world!";
 }
