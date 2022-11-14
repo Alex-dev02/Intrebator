de19f78e12f56762993a07521443c893dc85a33a
Zoinks <murarashuvlad@gmail.com>
2022-11-14 15:14:44 +0200

    remade the structure even doe we have a lot of boilerplate to anihilate

--- build.bat
+++
@@ -1,9 +0,0 @@
-if not exist build\ (
-    mkdir build 
-)
-
-cd build
-
-cmake .. -DCMAKE_TOOLCHAIN_FILE=C:\Users\ionut\OneDrive\Desktop\vcpkg\scripts\buildsystems\vcpkg.cmake
-cmake --build .
-cmake --install .
