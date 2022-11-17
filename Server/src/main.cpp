824d67021fc66062c067531433c485b127ad932c
Alex-dev02 <ionut02alex@yahoo.ro>
2022-11-17 17:09:53 +0200

    removed unused function

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -2,24 +2,14 @@
 
 #include "../include/Utils/Server.hpp"
 
-void n();
-
 int main()
 {
     Server server;
     auto& app = server.GetApp();
-    n();
+
     CROW_ROUTE(app, "/")([]() {
         return "Hello world";
         });
 
     server.Start();
-}
-
-void n() {
-    Server server;
-    auto& app = server.GetApp();
-    CROW_ROUTE(app, "/user")([]() {
-        return "Hello user";
-        });
 }
