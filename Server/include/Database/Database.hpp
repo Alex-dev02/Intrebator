d7805d2c159ef7998348f88022c5c7a51301a520
Alex-dev02 <ionut02alex@yahoo.ro>
2022-11-17 17:40:29 +0200

    vlad: functia function pentru testare baza de date

--- Server/include/Database/Database.hpp
+++ Server/include/Database/Database.hpp
@@ -1,9 +1,27 @@
 #pragma once
 
-#include <sqlite_orm/sqlite_orm>
+#include "sqlite_orm/sqlite_orm.h"
+
+
 
 class Database {
 
 
+    void function() {
+        using namespace sqlite_orm;
+
+        auto storage = make_storage("db.sqlite",
+            make_table("users",
+                make_column("id", &User::GetId, &User::SetId, autoincrement(), primary_key()),
+                make_column("first_name", &User::firstName),
+                make_column("last_name", &User::lastName),
+                make_column("birth_date", &User::birthDate),
+                make_column("image_url", &User::imageUrl),
+                make_column("type_id", &User::typeId)),
+            make_table("user_types",
+                make_column("id", &UserType::id, autoincrement(), primary_key()),
+                make_column("name", &UserType::name, default_value("name_placeholder"))));
+
+    }
 
 };
