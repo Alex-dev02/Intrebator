85226842420daa059e8d07a80b0628855f3f30cb
moldovanadrian03 <moldovan.adrian.alexandru03@gmail.com>
2022-11-22 05:13:08 -0800

    Implemented user table with specific columns for database.

--- Server/include/Database/Database.hpp
+++ Server/include/Database/Database.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "sqlite_orm/sqlite_orm.h"
+#include "../Entities/User.hpp"
 
 
 
@@ -11,16 +12,10 @@ class Database {
         using namespace sqlite_orm;
 
         auto storage = make_storage("db.sqlite",
-            make_table("users",
+            make_table("user",
                 make_column("id", &User::GetId, &User::SetId, autoincrement(), primary_key()),
-                make_column("first_name", &User::firstName),
-                make_column("last_name", &User::lastName),
-                make_column("birth_date", &User::birthDate),
-                make_column("image_url", &User::imageUrl),
-                make_column("type_id", &User::typeId)),
-            make_table("user_types",
-                make_column("id", &UserType::id, autoincrement(), primary_key()),
-                make_column("name", &UserType::name, default_value("name_placeholder"))));
+                make_column("name", &User::GetName, &User::SetId, unique())
+                ));
 
     }
 
