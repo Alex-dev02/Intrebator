ffbd2f1489bd6587a135180cc3ea0b50817e0005
petreastefann <petreastefann@gmail.com>
2022-11-24 14:53:49 +0200

    fix: fixed the function returning the current date and time

---
+++ Server/include/Entities/GenerateDateAndTime.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <ctime>
+
+class DateGenerator{
+public:
+	static const std::string& ReturnCurrentTimeAndDate();
+};
--- Server/src/Entities/GenerateDateAndTime.cpp
+++ Server/src/Entities/GenerateDateAndTime.cpp
@@ -1,10 +1,10 @@
 #include "../../include/Entities/GenerateDateAndTime.hpp"
 
-const std::string ReturnCurrentTimeAndDate(){
+const std::string& DateGenerator::ReturnCurrentTimeAndDate(){
 	time_t now = time(0);
-	struct tm tstruct;
-	char date_and_time[80];
-	tstruct = *localtime(&now);
-	strftime(date_and_time, sizeof(date_and_time), "%Y-%m-%d %X", &tstruct);
+	struct tm currenttime;
+	localtime_s(&currenttime, &now);
+	char date_and_time[40];
+	strftime(date_and_time, sizeof(date_and_time), "%Y-%m-%d %X", &currenttime);
 	return date_and_time;
 }
