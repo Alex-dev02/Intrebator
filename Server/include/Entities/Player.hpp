a7fa485649a20ff8d99b1043cc77b2e1d1e874eb
petreastefann <petreastefann@gmail.com>
2022-11-30 16:22:25 +0200

    added: partial declaration of player class

--- Server/include/Entities/Player.hpp
+++ Server/include/Entities/Player.hpp
@@ -2,17 +2,18 @@
 
 #include "User.hpp"
 
+#include <string>
+
 class Player : public User{
 public:
+	Player(uint32_t id, const std::string& name, char* color, uint32_t score);
 	// <---- an enum with red, green, yellow, blue here?
 
 	const std::string& GetColor() const;
-	const uint32_t GetScore() const;
-	// getter for inherited name?
+	uint32_t GetScore() const;
 	
 	void SetColor(char* color);
 	void SetScore(uint32_t score);
-	// setter for inherited name?
 private:
 	char* m_color;
 	uint32_t m_score;
--- Server/src/Entities/Player.cpp
+++ Server/src/Entities/Player.cpp
@@ -1,2 +1,23 @@
 #include "../../include/Entities/Player.hpp"
 
+Player::Player(uint32_t id, const std::string& name, char* color, uint32_t score)
+	:User(id, name), m_color(color), m_score(score)
+{}
+
+
+const std::string& Player::GetColor() const{
+	return m_color;
+}
+
+uint32_t Player::GetScore() const{
+	return m_score;
+}
+
+
+void Player::SetColor(char* color){
+	m_color = color;
+}
+
+void Player::SetScore(uint32_t score){
+	m_score = score;
+}
