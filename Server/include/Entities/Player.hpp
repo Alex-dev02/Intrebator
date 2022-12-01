d147a6cf462dfd64eff500488c060c793d397c29
moldovanadrian03 <moldovan.adrian.alexandru03@gmail.com>
2022-12-01 05:24:59 -0800

    Added an enum for colours in player.hpp and made a few changings for the functions

--- Server/include/Entities/Player.hpp
+++ Server/include/Entities/Player.hpp
@@ -4,18 +4,21 @@
 
 #include <string>
 
+
 class Player : public User{
 public:
-	Player(uint32_t id, const std::string& name, char* color, uint32_t score);
-	// <---- an enum with red, green, yellow, blue here?
+	enum Colour{RED, GREEN, YELLOW, BLUE};
 
-	const std::string& GetColor() const;
+	Player(uint32_t id, const std::string& name, Colour color, uint32_t score);
+	
+
+	const Colour& GetColor() const;
 	uint32_t GetScore() const;
 	
-	void SetColor(char* color);
+	void SetColor(Colour color);
 	void SetScore(uint32_t score);
 private:
-	char* m_color;
+	Colour m_color;
 	uint32_t m_score;
 	bool m_isActive = true;
 };
--- Server/src/Entities/Player.cpp
+++ Server/src/Entities/Player.cpp
@@ -1,11 +1,11 @@
 #include "../../include/Entities/Player.hpp"
 
-Player::Player(uint32_t id, const std::string& name, char* color, uint32_t score)
+Player::Player(uint32_t id, const std::string& name, Colour color, uint32_t score)
 	:User(id, name), m_color(color), m_score(score)
 {}
 
 
-const std::string& Player::GetColor() const{
+const Colour& Player::GetColor() const{ //nu stiu de ce nu gaseste Colour
 	return m_color;
 }
 
@@ -14,7 +14,7 @@ uint32_t Player::GetScore() const{
 }
 
 
-void Player::SetColor(char* color){
+void Player::SetColor(Colour color){
 	m_color = color;
 }
 
