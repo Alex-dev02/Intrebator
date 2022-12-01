cc0325c5a51fdcafff07fbe73d2b96219cd3ba85
moldovanadrian03 <moldovan.adrian.alexandru03@gmail.com>
2022-12-01 04:55:41 -0800

    Implemented setters and getters for Cell.cpp class

--- Server/src/Entities/Cell.cpp
+++ Server/src/Entities/Cell.cpp
@@ -1,15 +1,28 @@
 #include "../../include/Entities/Cell.hpp"
+#include <wrl/implements.h>
 
 Cell::Cell(uint16_t score, Cell::BaseType base_type)
 	: m_score(score),
 	m_base_type(base_type)
 {}
 
-// getscore
+uint16_t Cell::GetScore() const
+{
+	return m_score;
+}
 
-// getbasetype
+Cell::BaseType Cell::GetBaseType() const
+{
+	return m_base_type;
+}
 
+void Cell::SetScore(uint16_t score)
+{
+	m_score = score;
+}
 
-// set score
+void Cell::SetBaseType(BaseType base_type)
+{
+	m_base_type = base_type;
+}
 
-// setbasetype
