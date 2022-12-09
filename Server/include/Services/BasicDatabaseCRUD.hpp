355f5e6a38c627016c2db3a8a70c5d74ff341295
Alex-dev02 <ionut02alex@yahoo.ro>
2022-12-09 17:25:56 +0200

    added new methods in BasicDatabaseCRUD template class

--- Server/include/Services/BasicDatabaseCRUD.hpp
+++ Server/include/Services/BasicDatabaseCRUD.hpp
@@ -10,7 +10,12 @@ public:
 	BasicDatabaseCRUD(std::shared_ptr<Database> database)
 		:	m_database(database)
 	{}
-	T GetById(uint32_t id);
+	std::unique_ptr<T> GetById(uint32_t id) {
+		return std::move(m_database->get_pointer<T>(id));
+	}
+	void Insert(T data) {
+		m_database->insert(data);
+	}
 	std::vector<T> GetAll();
 	void ReplaceById(uint32_t id);
 
