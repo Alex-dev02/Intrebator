9fccedfb9f089ea1d87f9741255e7333fbaa206e
moldovanadrian03 <moldovan.adrian.alexandru03@gmail.com>
2022-12-22 18:03:38 +0200

    added: function for returning vector of multiple answer questions

--- Server/src/Utils/InsertFromFileIntoQuestions.cpp
+++ Server/src/Utils/InsertFromFileIntoQuestions.cpp
@@ -24,7 +24,31 @@ std::vector<Question*> InsertFromFileIntoQuestionsNum(){
 
 std::vector<Question*> InsertFromFileIntoQuestionsMultip(){
 	std::vector<Question*> questionsArray;
+	MultipleAnswerQuestion multipQuestion;
+	std::ifstream file1("numeric questions.txt");
+	std::string line;
+	int lineNb = 0;
 
+	while(std::getline(file1, line)){
+		if(lineNb % 6 == 0){
+			multipQuestion.SetQuestion(line);
+		}
+		if(lineNb % 6 == 1){
+			multipQuestion.SetFirstAnswer(line);
+		}
+		if(lineNb % 6 == 2){
+			multipQuestion.SetSecondAnswer(line);
+		}
+		if(lineNb % 6 == 3){
+			multipQuestion.SetThirdAnswer(line);
+		}
+		if(lineNb % 6 == 4){
+			multipQuestion.SetCorrectAnswer(line);
+			questionsArray.emplace_back(multipQuestion);
+		}
+		lineNb++;
+	}
 
+	file1.close();
 	return questionsArray;
 }
