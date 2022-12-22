13e59ddd1309045747e155f5136ec7958c13d9ca
moldovanadrian03 <moldovan.adrian.alexandru03@gmail.com>
2022-12-22 17:45:49 +0200

    added: function for returning vector of numerical questions

--- Server/src/Utils/InsertFromFileIntoQuestions.cpp
+++ Server/src/Utils/InsertFromFileIntoQuestions.cpp
@@ -1,2 +1,30 @@
 #include "../../include/Utils/InsertFromFileIntoQuestions.hpp"
 
+std::vector<Question*> InsertFromFileIntoQuestionsNum(){
+	std::vector<Question*> questionsArray;
+	NumericQuestion numQuestion;
+	std::ifstream file1("multiple answer questions.txt");
+	std::string line;
+	int lineNb = 0;
+
+	while(std::getline(file1, line)){
+		if(lineNb % 3 == 0){
+			numQuestion.SetQuestion(line);
+		}
+		else if(lineNb % 3 == 1){
+			numQuestion.SetAnswer(std::stoi(line));
+			questionsArray.emplace_back(numQuestion);
+		}
+		lineNb++;
+	}
+
+	file1.close();
+	return questionsArray;
+}
+
+std::vector<Question*> InsertFromFileIntoQuestionsMultip(){
+	std::vector<Question*> questionsArray;
+
+
+	return questionsArray;
+}
